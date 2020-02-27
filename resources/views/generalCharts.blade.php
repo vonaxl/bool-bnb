93daea4fca69f645b0dbe202dc5aae05345fa6b9
michele <michele.provolo@gmail.com>
2020-02-27 12:10:20 +0100

    charts
    migliorato responsive, aggiunta condizione se non ci sono apt

--- resources/views/generalCharts.blade.php
+++ resources/views/generalCharts.blade.php
@@ -2,18 +2,28 @@
 
 @section('content')
 <div class="container">
+    @if ($apartments->count() > 0)
     <div class="row align-items-center justify-content-around p-5">
+        
         <h2>Statistiche generali degli appartamenti</h2>
-        <div class="col-6">
-
-            <canvas id="myChart" width="400" height="400"></canvas>
+        <div class="col-12 col-md-9 col-md-offset-3 mt-3">
+            <canvas id="myChart" width="450" height="450"></canvas>
         </div>
+
     </div>
+        
+    @else
+    <h3>Statistiche non disponibili, in quanto non possiedi ancora nessun appartamento. </h3>
+    @endif
+    
+
     <script>
+        
         var apartments = @json($apartments);
         var messagesCount = @json($messagesCount);
         var apartmentsName = [];
         var apartmentsData = [];
+
         apartments.forEach(apartment => {
             apartmentsName.push(apartment['title']);
             apartmentsData.push(apartment['view']);
@@ -48,4 +58,5 @@
             }
         });
         </script>
+    
 @endsection
