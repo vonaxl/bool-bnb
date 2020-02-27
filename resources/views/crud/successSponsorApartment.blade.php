d7758c6a00a72dfadde05eab7a5642c88d19d548
Giampaolo1 <55143439+Giampaolo1@users.noreply.github.com>
2020-02-27 15:54:59 +0100

    FE Changes

--- resources/views/crud/successSponsorApartment.blade.php
+++ resources/views/crud/successSponsorApartment.blade.php
@@ -1,24 +1,30 @@
 @extends('layouts.app')
 @section('content')
-<div class="container sucsponsapt">
+<div class="container-fluid hero">
     <div class="row text-center mt-4 border rounded bg-success p-2">
         <div class="col-12">
-            <h1> - TRANSAZIONE AVVENUTA CON SUCCESSO - <br>
-                L'appartamento <b>{{$apartment -> title}}</b> è stato sponsorizzato e sarà visibile nella vetrina home per
-                @if ($apartment -> sponsored == 1)
-                24 ore!
-                @elseif($apartment -> sponsored == 2)
-                72 ore!
-                @elseif($apartment -> sponsored == 3)
-                144 ore!
-                @endif
+            <h1 class="text-white">
+                <i class="far fa-check-circle text-white"></i> Transazione avvenuta con successo
             </h1>
+        </div>
+    </div>
+    <div class="row text-center mt-4 border bg-light rounded p-2">
+        <div class="col-12">
+            <h3>L'appartamento <b>{{$apartment -> title}}</b> è stato sponsorizzato e sarà visibile nella vetrina home per
+                @if ($apartment -> sponsored == 1)
+                24 ore
+                @elseif($apartment -> sponsored == 2)
+                72 ore
+                @elseif($apartment -> sponsored == 3)
+                144 ore
+                @endif
+            </h3>
         </div>
     </div>
 
     <div class="row text-center mt-5">
         <div class="col-12">
-            <a class="btn btn-warning mb-3" href="{{route("home")}}" role="button">Torna alla home</a>
+            <a class="btn btn-light mb-3" href="{{route("home")}}" role="button">Torna alla home</a>
         </div>
     </div>
 
